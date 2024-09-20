d3303e6d628f74839dabbe0e1f70d8ede98c25e5
David <Davidsrei8@gmail.com>
2024-09-20 10:27:20 +0200

    Update exo python.py

--- exo python.py
+++ exo python.py
@@ -132,6 +132,22 @@ def est_parfait(num):
          if num % i==0:
              sum+=i
      return sum == 2*num
-
-#exo 19
-def 
+#jeu aleatoire
+import random
+def alea():
+    cible = random.randint(1,100)
+    trouve = 7
+    print('trouve un chiffre entre 1 a 100 en 7 fois')
+    
+    for i in range(trouve):
+        devine = int(input(f'Tu es a {i + 1}, met ton nombre '))
+        if devine < cible:
+            print('Le nombre est plus grand que le tiens')
+        elif devine > cible:
+            print('Le nombre est plus petit que le tiens')
+        else:
+            print('Vous avez trouve')
+            
+            break
+    else:
+        print(f'Finie !!. Le nombre etait: {cible}')
